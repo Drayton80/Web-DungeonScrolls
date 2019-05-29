748a6ad26bb09053406cc167c458fac8148ff2ce
Drayton80 <drayton80@hotmail.com>
2019-05-29 18:20:31 -0300

    v0.1.4 - Templates

--- DungeonScrolls/calculator/views.py
+++ DungeonScrolls/calculator/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 
 def experience_points(request):
+    context ={}
+
     return render(request, 'calculator/experience_points.html')
--- DungeonScrolls/dungeonscrolls/settings.py
+++ DungeonScrolls/dungeonscrolls/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'widget_tweaks',
     'calculator',
 ]
 
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'dungeonscrolls.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [(os.path.join(BASE_DIR, 'templates')),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
