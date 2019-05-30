12b3c2af76e5158069517d31880120b65df9262b
Drayton80 <drayton80@hotmail.com>
2019-05-30 16:45:34 -0300

    v0.1.6 - Small Improvements

--- DungeonScrolls/calculator/forms.py
+++ DungeonScrolls/calculator/forms.py
@@ -3,12 +3,13 @@ from .models import RuleSystem
 
 
 class FormSelectRuleSystem(forms.Form):
-    rule_system = forms.ChoiceField(choices=RuleSystem.objects.all(), label='Sistema de Regras',)
+    #rule_system = forms.ModelChoiceField(queryset=RuleSystem.objects.all(), label='Sistema de Regras',)
+    #rule_system = forms.ModelChoiceField(queryset=RuleSystem.objects.all(), label='Sistema de Regras', )
 
     class Meta:
-        fields = 'rule_system'
+        fields = ('rule_system', '')
 
     def __init__(self, rule_system_name, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.fields.queryset = RuleSystem.objects.filter(name=rule_system_name)
+        self.fields['rule_system'].queryset = RuleSystem.objects.filter(name=rule_system_name)
