c2524ea7a6bd03e51ed0cee04f681e5304433f58
Drayton80 <drayton80@hotmail.com>
2019-09-16 19:33:47 -0300

    v0.5.11 - REST Model Paths

--- DungeonScrolls/rest/urls.py
+++ DungeonScrolls/rest/urls.py
@@ -7,16 +7,16 @@ from . import views
 # router.register('api/getUser/<int:pk>/', views.UserDetail.as_view(), 'GetUser') isso é só pra viewsets
 
 urlpatterns = [
-     path('api/getUser/', views.UserDetail.as_view()),
-     path('api/getUser/<int:pk>/', views.UserDetail.as_view(), name='GetUser'),
-     path('api/getRuleSystem/<int:rule_system_id>/',
+     path('api/get-user/', views.UserDetail.as_view()),
+     path('api/get-user/<int:pk>/', views.UserDetail.as_view(), name='GetUser'),
+     path('api/get-rule-system/<int:rule_system_id>/',
           views.RuleSystemDetail.as_view(), name='GetRuleSystem'),
-     path('api/getBestiaryList/<int:user_id>/',
+     path('api/get-bestiary-list/<int:user_id>/',
           views.BestiaryList.as_view(), name='GetBestiaryList'),
-     path('api/getChapterList/<int:bestiary_id>/',
+     path('api/get-chapter-list/<int:bestiary_id>/',
           views.ChapterList.as_view(), name='GetChapterList'),
-     path('api/getSheetList/<int:chapter_id>/',
+     path('api/get-sheet-list/<int:chapter_id>/',
          views.SheetList.as_view(), name='GetSheetList'),
-     path('api/getSheetDnD35/<int:sheet_id>/',
+     path('api/get-sheet-dnd35/<int:sheet_id>/',
          views.SheetDnD35Detail.as_view(), name='GetSheetDnD35'),
 ]
