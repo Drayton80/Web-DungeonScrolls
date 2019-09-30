73d4bbf19f40dfcd473afd8753260780fe1ca5e1
ArmandoTGT <armando_tgt@hotmail.com>
2019-09-30 17:39:54 -0300

    v0.8.2 Settings POST DATA

--- DungeonScrolls/settings_page/views.py
+++ DungeonScrolls/settings_page/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 def settings_page(request):
-    return render(request, 'settings_page/settings_page.html')
+    return render(request, 'settings_page/settings_page.html')    
 
